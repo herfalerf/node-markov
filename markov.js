4476222ee820a18f140362a538e132bbbaaccd06
herfalerf <jpendy@gmail.com>
2021-04-28 23:35:18 -0500

    done

--- markov.js
+++ markov.js
@@ -17,17 +17,21 @@ class MarkovMachine {
   makeChains() {
     let chains = new Map();
 
+    //this loops through words array.
     for (let i = 0; i < this.words.length; i += 1) {
       let word = this.words[i];
       let nextWord = this.words[i + 1] || null;
 
+      //if the word is already in chains, get the next word and add to chains
       if (chains.has(word)) chains.get(word).push(nextWord);
+      //if the word is not in chains, set the word as a key in chains and the next word as its value
       else chains.set(word, [nextWord]);
     }
 
     this.chains = chains;
   }
 
+  // function to choose a random word from a passed in array
   static choice(ar) {
     return ar[Math.floor(Math.random() * ar.length)];
   }
